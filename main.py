5121f731004f90b7d5b4354554b7bd70ff85ccce
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-06-09 20:13:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ success_rate = 0.5
 trials = range(1, 10000)
 outcome_list = []
 
-#iterate through expirements
+#iterate through experiments
 for i in range(0, len(trials)):
     outcome_list.append(coin_flips.trial_sequence(trials[i], success_rate))
 
-#generate outcome
+#generate outcome plot
 plt.scatter(*zip(*outcome_list))
 plt.suptitle('Coin Flip Simlation Scatter Plot')
 plt.title('Realized Distance from Expected Success Rate in Percent')
