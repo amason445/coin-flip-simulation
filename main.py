d2b4454732b218033cc6f2d24e7b84ead5d36a2d
Alex Mason <77219320+amason445@users.noreply.github.com>
2023-06-13 20:25:08 -0600

    Added a second simulation problem

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import coin_flips
-import matplotlib.pyplot as plt
+import simulation1
+import simulation2
 
-success_rate = 0.5
-trials = range(1, 10000)
-outcome_list = []
+try:
+    simulation1.main()
+    print('Simulation 1 is complete!')
+    simulation2.main()
+    print('Simulation 2 is complete!')
+except Exception as e:
+    print('Error: ', e)
 
-#iterate through experiments
-for i in range(0, len(trials)):
-    outcome_list.append(coin_flips.trial_sequence(trials[i], success_rate))
-
-#generate outcome plot
-plt.scatter(*zip(*outcome_list))
-plt.suptitle('Coin Flip Simlation Scatter Plot')
-plt.title('Realized Distance from Expected Success Rate')
-plt.xlabel('Number of Trials')
-plt.ylabel('Delta as a Percentage')
-plt.savefig('output_plot.png')
 
 
---
+++ simulation2.py
@@ -0,0 +1,30 @@
+import coin_flips
+import matplotlib.pyplot as plt
+
+def main():
+    success_rate = 0.5
+    expirements = range(1,100 + 1)
+    trials = range(1, 20 + 1)
+    outcome_list = []
+    
+    #iterate through experiments
+    for i in range(0, len(expirements)):
+        for j in range(0, len(trials)):
+            trial_outcome = coin_flips.trial_sequence(trials[j], success_rate)
+            outcome_list.append(trial_outcome)
+
+    outcome_filter = lambda x: True if x[0] == 20 else False
+    outcome_list = list(filter(outcome_filter, outcome_list))
+
+    outcome_counts = [i[1] for i in outcome_list]
+  
+    # generate outcome plot
+    plt.hist(outcome_counts)
+    plt.suptitle('Coin Flip Histogram')
+    plt.title('Number of Heads Observed in 100 Sequences of 20 Coin Flips')
+    plt.xlabel('Number of Heads')
+    plt.ylabel('Frequency')
+    plt.savefig('simulation2_plot.png')
+
+if __name__ == '__main__':
+    main()
